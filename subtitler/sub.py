7bfc32119c1469bdd2f211c47ee6eef5708d67ef
OVH <hector.buffiere@ens.fr>
2022-01-27 20:25:00 +0000

    subtitles almost work

--- subtitler/sub.py
+++ subtitler/sub.py
@@ -19,23 +19,25 @@ with open('duration') as f:
     lines = f.readlines()
     duration = float(lines[0])
 
-p = "/home/jovyan/projet-ml/subtitler/audio-"
-paths = []
+p = "/home/jovyan/projet-ml/subtitler/audio.wav"
+paths = [p]
 
-os.system("rm audio-*.wav")
+os.system("rm audio*.wav")
 
 n = int(duration/5)+1
-
-for i in range(n):
-    os.system("ffmpeg -i out -acodec pcm_s16le -ac 1 -ar 16000 audio-{0}.wav".format(i))
-    paths.append(p+str(i)+'.wav')
-    
+os.system("ffmpeg -i out -acodec pcm_s16le -ac 1 -ar 16000 audio0.wav")
+os.system("ffmpeg -f concat -safe 0 -i concat audio.wav")
+ 
 #Get the transcription
 hyp = model.transcribe(paths, return_hypotheses=True)[0][0]
 
 #Split it into words with timesteps
-timed_words = []
 
+lines = ['00:00:00,000 --> 00:00:05,000\n', '']
+
+tmax = hyp.timestep[-1]
+
+timed_words = []
 for token, t in zip(hyp.y_sequence, hyp.timestep):
     syl = model.decoding.decode_ids_to_tokens([int(token)])[0]
     if syl[0]=='▁':
@@ -44,5 +46,17 @@ for token, t in zip(hyp.y_sequence, hyp.timestep):
         w,b,_ = timed_words[-1]
         timed_words[-1] = (w+syl, b, t)
 
+extract = 0
+for (w, b, e) in timed_words:
+    idx_extract = (n*e)//tmax
+    if idx_extract > extract:
+        extract += 1
+        i = extract
+        lines.append('\n\n00:{0}:{2},000 --> 00:{1}:{3},000\n'.format(str(i//12%60).zfill(2), str((i+1)//12%60).zfill(2),
+            str(5*i%60).zfill(2), str(5*(i+1)%60)).zfill(2))
+        lines.append('')
+    lines[-1] += w + " "
 
-print(timed_words)
+with open('caps.srt', 'w') as f:
+    for line in lines:
+        f.write(line)
